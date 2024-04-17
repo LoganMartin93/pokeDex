16e19a83098b06388392fd830c57c033e9de4091
LoganMartin93 <166079175+LoganMartin93@users.noreply.github.com>
2024-04-17 13:59:14 -0700

    update three pokemon objects for array pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello World');
-let favoriteFood = 'Steak';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Bulbasaur', height: 7, types: 'Grass'},
+    {name: 'Charmander', height: 6, types: 'Fire'},
+    {name: 'Pikachu', height: 4, types: 'Electric'}
+];
