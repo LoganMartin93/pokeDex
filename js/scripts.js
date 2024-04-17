c555e9e9c779f4be936291ad1d482dca457ab162
LoganMartin93 <166079175+LoganMartin93@users.noreply.github.com>
2024-04-17 14:40:31 -0700

    add Charizard to pokemonList and print list
    added a conditional in the for loop that is iterating through pokemonList to print "Wow, that's big!" for any height over 1.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 let pokemonList = [
-    {name: 'Bulbasaur', height: 7, types: 'Grass'},
-    {name: 'Charmander', height: 6, types: 'Fire'},
-    {name: 'Pikachu', height: 4, types: 'Electric'}
+    {name: 'Bulbasaur', height: 0.7, types: 'Grass'},
+    {name: 'Charmander', height: 0.6, types: 'Fire'},
+    {name: 'Pikachu', height: 0.4, types: 'Electric'},
+    {name: 'Charizard', height: 1.7, types: 'Fire, Flying'}
+
 ];
+
+for(let i = 0; i < 4; i++){
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
+    if(pokemonList[i].height > 1){
+        document.write("Wow, that's big!");
+    }
+    document.write("<br>");
+}
