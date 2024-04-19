f6102c0e44efb0bca830981d03c2e6750f65cd42
LoganMartin93 <166079175+LoganMartin93@users.noreply.github.com>
2024-04-18 20:00:28 -0700

    IIFE was added with add/getAll keys.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,31 @@
-let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, types: 'Grass'},
-    {name: 'Charmander', height: 0.6, types: 'Fire'},
-    {name: 'Pikachu', height: 0.4, types: 'Electric'},
-    {name: 'Charizard', height: 1.7, types: 'Fire, Flying'}
+let pokemonRepository = (function(){
+    let pokemonList = [
+        {name: 'Bulbasaur', height: 0.7, types: 'Grass'},
+        {name: 'Charmander', height: 0.6, types: 'Fire'},
+        {name: 'Pikachu', height: 0.4, types: 'Electric'},
+        {name: 'Charizard', height: 1.7, types: 'Fire, Flying'}
 
-];
+    ];
 
-pokemonList.forEach(function(pokemon){
+    function add(pokemon){
+        pokemonList.push(pokemon);
+    }
+
+    function getAll(){
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon){
     document.write(pokemon.name + ' (height: ' + pokemon.height + ')');
     if(pokemon.height > 1){
-        document.write("Wow, that's bi!");
+        document.write("Wow, that's big!");
     }
     document.write("<br>");
 });
